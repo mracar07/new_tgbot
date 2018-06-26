8f61366d71e0aebdf9de7068c3ec3f9a3a77b518
mr.acar07 <mr.acar07@gmail.com>
2018-06-26 20:40:12 +0300

    Translate AFK module to TR

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -22,7 +22,7 @@ def afk(bot: Bot, update: Update):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    update.effective_message.reply_text("{} is now AFK!".format(update.effective_user.first_name))
+    update.effective_message.reply_text("{} şuan meşgul!".format(update.effective_user.first_name))
 
 
 @run_async
@@ -34,7 +34,7 @@ def no_longer_afk(bot: Bot, update: Update):
 
     res = sql.rm_afk(user.id)
     if res:
-        update.effective_message.reply_text("{} is no longer AFK!".format(update.effective_user.first_name))
+        update.effective_message.reply_text("{} artık meşgul değil!".format(update.effective_user.first_name))
 
 
 @run_async
@@ -61,20 +61,20 @@ def reply_afk(bot: Bot, update: Update):
             if sql.is_afk(user_id):
                 user = sql.check_afk_status(user_id)
                 if not user.reason:
-                    res = "{} is AFK!".format(fst_name)
+                    res = "{} Meşgul!".format(fst_name)
                 else:
-                    res = "{} is AFK! says its because of:\n{}".format(fst_name, user.reason)
+                    res = "{} Meşgul! Sebep:\n{}".format(fst_name, user.reason)
                 message.reply_text(res)
 
 
 __help__ = """
- - /afk <reason>: mark yourself as AFK.
- - brb <reason>: same as the afk command - but not a command.
+ - /afk <sebep>: Kendini meşgul olarak işaretle.
+ - brb <sebep>: AFK komutu ile aynı - ama bir komut değil.
 
-When marked as AFK, any mentions will be replied to with a message to say you're not available!
+Meşgul olarak işaretlendiğinizde, herhangi bir kişi sizinle etkileşime girmeye çalışırsa meşgul olduğunuzu belirten bir mesajla yanıtlanır!
 """
 
-__mod_name__ = "AFK"
+__mod_name__ = "Meşgul"
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
 AFK_REGEX_HANDLER = DisableAbleRegexHandler("(?i)brb", afk, friendly="afk")
